c3218191260e814f12a2d6a59a6b35d9691e7272
Jiniji <96576269+Jiniji@users.noreply.github.com>
2024-11-14 18:14:20 +0100

    Django Setup

--- Softwarepraktikum/Softwarepraktikum/settings.py
+++ Softwarepraktikum/Softwarepraktikum/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Softwarepraktikum.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db',
+        'USER': 'postgres',
+        'PASSWORD': 'jiniji',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
