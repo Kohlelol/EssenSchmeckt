f745ca34fce87ea543b4be026ec258bea26e36cc
Jiniji <96576269+Jiniji@users.noreply.github.com>
2024-11-15 11:27:50 +0100

    database testing

--- Softwarepraktikum/database/models.py
+++ Softwarepraktikum/database/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=30)
+    date_of_birth = models.DateField()
+    qr_code = models.CharField(max_length=30)
+    status = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
