08ae285252429a29eac9aa4cb8589d67a4a89fe8
Jiniji <96576269+Jiniji@users.noreply.github.com>
2024-11-15 11:11:31 +0100

    renamed groupleader to database

--- Softwarepraktikum/Softwarepraktikum/urls.py
+++ Softwarepraktikum/Softwarepraktikum/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage),
     path('login/', views.login),
-    path('groupleader/', include('groupleader.urls')),
+    path('database/', include('database.urls')),
 ]
--- Softwarepraktikum/database/views.py
+++ Softwarepraktikum/database/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def groupmember_list(request):
-    return render(request, 'groupleader/groupmember_list.html')
+    return render(request, 'database/database_list.html')
