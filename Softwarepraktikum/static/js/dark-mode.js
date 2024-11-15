98012966f13e604950d035064d515ba8c112432a
Jiniji <96576269+Jiniji@users.noreply.github.com>
2024-11-15 11:01:47 +0100

    template rewrite working this time

---
+++ Softwarepraktikum/static/js/dark-mode.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('dark-mode-toggle');
+    const body = document.body;
+
+    if (localStorage.getItem('dark-mode') === 'enabled') {
+        body.classList.add('dark-mode');
+        toggleButton.checked = true;
+    } else {
+        body.classList.remove('dark-mode');
+        body.classList.add('light-mode');
+        toggleButton.checked = false;
+    }
+
+    document.getElementById('body').style.display = 'block';
+
+    toggleButton.addEventListener('change', () => {
+        body.classList.toggle('dark-mode');
+        body.classList.toggle('light-mode');
+
+        if (body.classList.contains('dark-mode')) {
+            localStorage.setItem('dark-mode', 'enabled');
+        } else {
+            localStorage.setItem('dark-mode', 'disabled');
+        }
+    });
+});
