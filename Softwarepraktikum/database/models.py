614315bb5a8258ee9fd7480874d7edb46f710d48
Jiniji <96576269+Jiniji@users.noreply.github.com>
2024-11-20 20:26:22 +0100

    UUID now has a default

--- Softwarepraktikum/database/models.py
+++ Softwarepraktikum/database/models.py
@@ -5,7 +5,7 @@ import qrcode
 # Create your models here.
 
 class Person(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid16, editable=False)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=30)
     date_of_birth = models.DateField()
